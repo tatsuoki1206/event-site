0600d2ecd3ed56c6a5d7a34119e4f9d066e8daab
福田 龍宙 <tatsuokifukuda@fukudanoMacBook-Pro.local>
2024-06-07 03:00:35 +0900

    確認画面から戻り時とエラー時の値を保持

--- resources/views/edit/edit_form.blade.php
+++ resources/views/edit/edit_form.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 
-<form class="form-signin" method="POST" action="{{ route('edit') }}">
+<form class="form-signin" method="POST" action="{{ route('editConfirm') }}">
     @csrf
   <h1 class="h3 mb-3 font-weight-normal">ユーザ編集フォーム</h1>
 
@@ -21,19 +21,19 @@
       <li>{{$error}}</li>
     </ul>
   @endforeach
-        
+  
   <x-alert type="danger" :session="session('danger')" />
-  <input type="hidden" name="id" value="{{ Auth::user()->id }}">
-  <label for="inputEmail" class="sr-only">名前（姓・名）</label>
-  <input type="text" id="inputName" name="name" class="form-control" placeholder="Last Name  First Name" value="{{ Auth::user()->name }}" required autofocus>
-  <label for="inputEmail" class="sr-only">メールアドレス</label>
-  <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" value="{{ Auth::user()->email }}" required autofocus>
-  <label for="inputPassword" class="sr-only">パスワード</label>
+  <input type="hidden" name="id" value="{{ $inputs->id }}">
+  <label for="inputEmail">名前（姓・名）</label>
+  <input type="text" id="inputName" name="name" class="form-control" placeholder="Last Name  First Name" value="{{ old('name') ?? $inputs->name }}" required autofocus>
+  <label for="inputEmail">メールアドレス</label>
+  <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" value="{{ old('email') ?? $inputs->email }}" required autofocus>
+  <label for="inputPassword">パスワード</label>
   <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-  <label for="inputPassword" class="sr-only">パスワード（確認用）</label>
+  <label for="inputPassword">パスワード（確認用）</label>
   <input type="password" id="inputPassword_confirm" name="password_confirm" class="form-control" placeholder="Password" required>
-  <a class="btn btn-lg btn-primary btn-block" href="/">戻る</a>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">更新</button>
+  <a class="btn btn-lg btn-primary btn-block" href="/">ホームに戻る</a>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">確認画面へ</button>
 </form>
 
 </body>
