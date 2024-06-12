af7ae17f3cd004febad42eb7d97121f285ec9bf8
tatsuoki1206 <fukuwazyutsu6021@gmail.com>
2024-06-13 01:22:58 +0900

    パスワード再設定機能作成完了 6/13

--- app/Models/Password_reset_token.php
+++ app/Models/Password_reset_token.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class Password_reset_token extends Model
 {
